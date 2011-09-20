07556301d903c08ac421673ead70ef744a77de50
Douglas Campos <qmx@qmx.me>
2011-09-20 01:18:08 -0300

    initial skeleton

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org"
+
+# Specify your gem's dependencies in jruby-management.gemspec
+gemspec
---
+++ lib/jruby-management.rb
@@ -0,0 +1,7 @@
+require "jruby-management/version"
+
+module Jruby
+  module Management
+    # Your code goes here...
+  end
+end
