f5a9d8dd1b1ad36ea632adb2d412dfc3d330b4d7
Douglas Campos <qmx@qmx.me>
2011-11-11 01:02:27 -0200

    DRY

--- lib/jruby-management.rb
+++ lib/jruby-management.rb
@@ -5,6 +5,10 @@ require "jmx4r"
 module JRuby
   module Management
     class ProfilerMBean < JMX::DynamicMBean
+      class << JRuby.runtime
+        field_reader :config
+      end
+
       operation "dumps profiler info to specified file"
       parameter :string, "path", "path to write profiling results"
       returns :void
@@ -24,9 +28,6 @@ module JRuby
       operation "enables profiler"
       returns :void
       def enable_profiling
-        class << JRuby.runtime
-          field_reader :config
-        end
         JRuby.runtime.config.profiling_mode = org.jruby.RubyInstanceConfig::ProfilingMode::GRAPH
         JRuby.reference(JRuby.runtime.kernel).invalidate_cache_descendants
       end
